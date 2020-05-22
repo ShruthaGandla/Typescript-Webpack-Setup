d44dd8bc3cb5a8d94985c140eb50d1f4f8e0cb89
Gandla <gandlas@it-c00121.twpn.root.washpost.com>
2020-05-22 15:13:46 -0400

    export App react component for end users consumption

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // any functions that we want to make available to end users should be exported like this in index.tsx(entry file)
 export { Add }  from './Addition';
+export * from './App';
 
 
 ReactDOM.render(<App name='shrutha'/>, document.getElementById('root'))
